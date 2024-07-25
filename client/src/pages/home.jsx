fbac8a39f441f49d19d3532efc7fbc717766c105
Odd-Doc <86434122+Odd-Doc@users.noreply.github.com>
2024-07-25 14:59:17 +0000

    prettier formating and navbar

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar";
+
+const Home = () => {
+  return (
+    <div className="root-container">
+      <h1 className="home-title">Welcome to Doc's Portfolio!</h1>
+      <Navbar />
+    </div>
+  );
+};
+export default Home;
