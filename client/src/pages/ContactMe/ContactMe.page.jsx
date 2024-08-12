1adcb91f890615e67c181d687d414ea01e1d19a5
Odd-Doc <gsphillipsjr@gmail.com>
2024-08-12 16:10:25 -0500

    form component name

--- client/src/pages/ContactMe/ContactMe.page.jsx
+++ client/src/pages/ContactMe/ContactMe.page.jsx
@@ -9,7 +9,7 @@ export const ContactMePage = () => {
         <Logo to="/">GP_</Logo>
       </Navbar>
       <Container>
-        <Form name="contact" netlify>
+        <form name="contact" netlify>
           <input placeholder="name*" type="text" name="name" required={true} />
           <input
             placeholder="email address*"
@@ -24,7 +24,7 @@ export const ContactMePage = () => {
             required={true}
           />
           <button type="submit">Submit</button>
-        </Form>
+        </form>
       </Container>
     </>
   );
