2428c4f529e96e5f212ace9f151f00f07ccc174a
Odd-Doc <gsphillipsjr@gmail.com>
2024-08-19 12:50:19 -0500

    side bar contact added

--- client/src/components/sideBar/style.jsx
+++ client/src/components/sideBar/style.jsx
@@ -26,19 +26,34 @@ const slideInLeft = keyframes`
     transform: translateX(0%)
   }
 `;
-
+export const TriggerWrapper = styled.div`
+  position: absolute;
+  && .in {
+    height: 125vh;
+    transform: translateY(96em);
+  }
+`;
 export const Container = styled.div`
-  /* color: ${(props) => props.theme.colors.platinum}; */
-  display: flex;
-  justify-content: center;
   position: fixed;
-  top: 0%;
+  top: 50%;
+  left: ${(props) => (props.theme.inView ? "0%" : "-100%")};
+  transition: ease-in-out 0.5s;
   align-items: center;
-  flex-direction: column;
-  /* background-color: ${(props) => props.theme.colors.platinum}; */
-  background-color: aquamarine;
 
+  background-color: ${(props) => props.theme.colors.orangeweb};
+  padding: 1.2rem;
+  border-radius: 0px 8px 8px 0px;
   ${media.sm`
     font-size: 1em;
-  `}
+  `};
+  .icon-group {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
+export const InViewTrigger = styled.div`
+  /* position: absolute;
+  top: 70%;
+  height: 10px; */
 `;
